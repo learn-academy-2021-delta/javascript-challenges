27199247318d178989226afb62d3b748aa108b56
sjproctor <sjproctor950@gmail.com>
2021-09-09 13:43:11 -0700

    adding additional js intro coding challenges

--- intro-austin-sarah.js
+++ intro-austin-sarah.js
@@ -11,3 +11,15 @@ console.log(34 + 71)
 // Write the code that will log whether your string includes the letter "e"?
 // Write the code that will log the character at the first index of the string.
 // Write the code that will log the string in all uppercase letters.
+
+
+// Is 34 divided by 3 greater than 67 divided by 2?
+// Does 5 evaluate to the same as "5"?
+// Does 5 strictly equal "5"?
+// Does !3 strictly equal 3?
+// Does "LEARN".length strictly equal 5 AND "Student".length strictly equal 7?
+// Does "LEARN".length strictly equal 5 OR "Student".length strictly equal 10?
+// Does "LEARN" contain the subset "RN"?
+// Does "LEARN" contain the subset "rn"?
+// Does "LEARN"[0] strictly equal "l"?
+// Modify the code from the previous question to return true.
