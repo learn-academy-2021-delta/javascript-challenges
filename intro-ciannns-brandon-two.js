9d4a6823ff42634d7b1e2d83174829835bc6a10d
ciannadogan <dogan.cianna@yahoo.com>
2021-09-09 15:29:36 -0700

    Completed remaining challnge questions

--- intro-ciannns-brandon-two.js
+++ intro-ciannns-brandon-two.js
@@ -46,9 +46,19 @@ console.log("LEARN"[0] === ("l"))
 console.log("LEARN"[0] === ("L"))
 
 // Write the code that will log theAnswer divided by 2.
+var theQuestion = "life, the universe, and everything"
+var theAnswer = 42
+console.log(theAnswer / 2)
 // Write the code that will log the length of theQuestion.
+console.log(theQuestion.length)
 // Write the code that will log the index of the character "f" in the theQuestion.
+console.log(theQuestion.indexOf("f"))
 // Write the code that will log the concatenation of the two variables.
 // Write the code that will log "the universe".
+console.log(theQuestion.slice(6,18))
 // Write the code that will log the character "l" from theQuestion.
+if(theQuestion === "life, the universe, and everything"){
+  console.log("l")
+}
 // Write the code that will log whether theQuestion.length is greater than theAnswer.
+console.log((theQuestion.length) > theAnswer)
