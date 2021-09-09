d178d521228f4facc3641da11f587e7b8dc7939a
bobobnj <robertmathewcook@gmail.com>
2021-09-09 14:52:58 -0700

    conditionals-rob-phil.js

---
+++ intro-rob-phil.js
@@ -0,0 +1,10 @@
+console.log(33 + 71)
+123 - 67
+56 * 23
+45 / 5
+5 ** 7
+33 % 6
+console.log("rob".length)
+console.log("rob".includes(e))
+console.log("rob".charAt(0))
+console.log("rob".toUpperCase())
