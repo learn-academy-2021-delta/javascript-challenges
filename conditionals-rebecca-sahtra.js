5140892124afc1547a34b7b1d7f061abbb6468b0
LiangchengR <liangchengr@gmail.com>
2021-09-09 16:14:56 -0700

    completed questions 5-7 of the challenges

--- conditionals-rebecca-sahtra.js
+++ conditionals-rebecca-sahtra.js
@@ -36,10 +36,49 @@ if (bigGuy === littleGuy){
   console.log(littleGuy);
 }
 // Write a statement that takes a variable of a number and logs whether the number is odd, even, or zero.
-//
+console.log("Question 5");
+
+var num = 39;
+
+if(num === 0){
+  console.log("This number is zero.");
+} else if (num%2 === 0){
+  console.log("This number is even!");
+} else{
+  console.log("This number is odd!");
+}
+
 // STRETCH Challenges
 // Write a statement that takes a variable of a grade percentage and logs the letter grade for that percentage, if the grade is 100% log "perfect score", if the grade is zero log "no grade available."
-//
+console.log("Question 6");
+
+var gradePercent = 100;
+
+if(gradePercent === 100){
+  console.log("A+, Perfect Score! Genius!");
+} else if(gradePercent === 0){
+  console.log("No Grade Available");
+} else if(gradePercent < 100 && gradePercent >= 90){
+  console.log("You got an A. But not an A+, terrible.");
+} else if(gradePercent < 90 && gradePercent >= 80){
+  console.log("You got an B.");
+} else if(gradePercent < 80 && gradePercent >= 70){
+  console.log("You got an C.");
+} else if(gradePercent < 70 && gradePercent >= 65){
+  console.log("You got an D.");
+} else if(gradePercent < 65){
+  console.log("YOU FAILED")
+} else {
+  console.log("Outside of the grading scale!");
+}
+
 // Write a statement that takes a variable of a boolean, number, or string data type and logs the data type of the variable. HINT: Check out the JavaScript typeof operator.
-//
+console.log("Question 7");
+
+var random = true;
+
+ if(typeof random === "boolean"){
+  console.log(typeof temp);
+ }
+
 // Create a password checker using a single conditional statement. If a user inputs a password with 12 or more characters AND the password includes !, then log "That is a mighty strong password!" If the user’s password is 8 or more characters OR includes !, then log "That password is strong enough." Log "That is not a valid password." for every other input.
