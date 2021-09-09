48b76a5d3ef3b9185b80cb644fc1f9de97d514e9
ciannadogan <dogan.cianna@yahoo.com>
2021-09-09 11:33:28 -0700

    completed 11-15 of challenge questions

--- intro-ciannns-brandon-two.js
+++ intro-ciannns-brandon-two.js
@@ -21,13 +21,19 @@ console.log(greeting.includes("e"))
 console.log("Hello"[0])
 // Write the code that will log the string in all uppercase letters.
 console.log(greeting.toUpperCase())
+
 // Write the code that will log true or false for the following:
 //
 // Is 34 divided by 3 greater than 67 divided by 2?
+console.log(34 / 3 > 67 / 2)
 // Does 5 evaluate to the same as "5"?
+console.log(5 == "5")
 // Does 5 strictly equal "5"?
+console.log(5 === "5")
 // Does !3 strictly equal 3?
+console.log(!3 === 3)
 // Does "LEARN".length strictly equal 5 AND "Student".length strictly equal 7?
+console.log((("LEARN".length) === 5) || ("STUDENT".length) === 7)
 // Does "LEARN".length strictly equal 5 OR "Student".length strictly equal 10?
 // Does "LEARN" contain the subset "RN"?
 // Does "LEARN" contain the subset "rn"?
