c88de6a31b37db08ba091e248ada1d88aea719a3
youngmarisa <marisayoung2013@gmail.com>
2021-09-13 16:48:30 -0700

    completed loops Q2-3

--- loops-marisa-kevin.js
+++ loops-marisa-kevin.js
@@ -2,23 +2,65 @@
 
 // for (let i = 1; i <= 20; i++) {
 //     console.log(i);
-    
+
 // }
 
 // Write a for loop that logs the result of each number from 1 - 20 tripled.
 
 // for (let i = 1; i <= 20; i++) {
 //     console.log(i * 3);
-    
+
 // }
 
 // Create a for loop that logs each even number from 1-20, and in the place of every odd number, returns the word "ODD" Expected output --> ODD, 2, ODD, 4, ODD, 6 ...etc
 
 // var numbers = 20
-for (let i = 0; i <= 20; i++) {
-    if (i % 2 == 0) {
-        console.log(i);
-    } else {
-        console.log("ODD"); 
-    }
+// for (let i = 0; i <= 20; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i);
+//     } else {
+//         console.log("ODD");
+//     }
+// }
+
+
+// Looping over an array. Consider this variable:
+var nums = [3, 57, -9, 20, 67]
+// Create a loop that will log the highest number from the array. Expected output --> 67
+
+
+// for(let i=0; i <nums.length-4; i++){
+//   nums.sort((a,b)=>a-b);
+//   console.log(nums[4]);
+// }
+
+
+// Create a loop that will log the lowest number from the array Expected output --> -9
+
+// for(let i=0; i <nums.length-4; i++){
+//   nums.sort((a,b)=>a-b);
+//   console.log(nums[0]);
+// }
+
+
+// Create a loop that will log the remainder of each number when divided by 2.
+// Expected output --> 1, 1, -1, 0, 1
+
+
+// for(let i=0; i <nums.length; i++){
+//   console.log(nums[i] %= 2);
+// }
+
+var myString = "learn student"
+// Write the code that will log the number of times the letter "e" occurs in the string.
+// Expected output --> 2
+
+
+
+let count = 0
+
+for (let i = 0; i < myString.length; i++) {
+  if(myString(i) == 'e') count++ {
+    console.log(count);
+  }
 }
