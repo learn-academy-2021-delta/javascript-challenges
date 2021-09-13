f5c6b9a973bf7be051068484ef91c181c9b8db6f
JBrady <jbradysd@icloud.com>
2021-09-13 16:03:48 -0700

    updating code for example 3

--- loop-et-jb.js
+++ loop-et-jb.js
@@ -11,13 +11,14 @@
 
 
 // Create a for loop that logs each even number from 1-20, and in the place of every odd number, returns the word "ODD" Expected output --> ODD, 2, ODD, 4, ODD, 6 ...etc
-// let num = 20
-// for (let i=1; i<=20; i++) {  
-//     console.log("ODD")
-//     if (num % 2 === 0) {
-//         console.log(i)
-//     }
-// }
+//let num = 20
+for (let i=1; i<=20; i++) {  
+    if (i % 2 !== 0) {
+        console.log("ODD")
+    } else if (i % 2 === 0) {
+        console.log(i)
+    }
+}
 
 
 // Looping over an array. Consider this variable:
@@ -71,14 +72,14 @@
 // }
 
 // Fizz Buzz: Use a for loop to log all numbers from 1-100. If a number is a multiple of 3, replace it with the word fizz. If a number is a multiple of five, replace it with the word buzz. If a number is a multiple of both 3 and 5, replace it with fizzbuzz. Expected output --> 1, 2, "fizz", 4, "buzz", "fizz", 7, 8, "fizz", "buzz", 11, "fizz", 13, 14, "fizzbuzz" ...etc
-for(let i=1; i<=100; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log("fizzbuzz")
-    } else if (i % 3 === 0) {
-        console.log("fizz")
-    } else if (i % 5 === 0) {
-        console.log("buzz")
-    } else {
-        console.log(i)
-    }
-}
+// for(let i=1; i<=100; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         console.log("fizzbuzz")
+//     } else if (i % 3 === 0) {
+//         console.log("fizz")
+//     } else if (i % 5 === 0) {
+//         console.log("buzz")
+//     } else {
+//         console.log(i)
+//     }
+// }
