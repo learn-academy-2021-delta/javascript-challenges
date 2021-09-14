a4388264dd698f7173cfa9643c1c42ab2a3cfcab
brandonho2000 <bradho099@gmail.com>
2021-09-14 16:47:43 -0500

    worked on a little more for practice

--- functions-ashley-brandon.js
+++ functions-ashley-brandon.js
@@ -14,23 +14,40 @@
 
 // Write a function named oddOrEven that takes a number as an argument and returns whether the number is odd or even.
 
-const oddOrEven = (number)=> {
-    if(oddOrEven % 2 == 0){
-        return `even` 
-    } else{
-        return `odd`
-    } 
-}
-console.log(oddOrEven(12));
+// const oddOrEven = (number)=> {
+//     if(oddOrEven % 2 == 0){
+//         return `even`
+//     } else{
+//         return `odd`
+//     }
+// }
+// console.log(oddOrEven(12));
 
 
 // Write a function named triple that takes a number as an argument and returns the result of that number multiplied by 3.
 
-
+// const tripple =(number) => {
+//   return number * 3
+// }
+// console.log(tripple(9))
 
 // Write a function named multiply that takes two numbers as arguments and returns the result of the numbers multiplied together.
 
+// const multiply = (num1, num2) => {
+//   return num1 * num2
+// }
+// console.log(multiply(3, 5))
+
 // Write a function named divisibleBy that takes two numbers as arguments and returns whether the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5".
+
+const divisibleBy = (num1, num2) => {
+  if(num1 / num2 % 2 === 0){
+  return `${num1} is evenly divisible by ${num2}`
+} else {
+  return "hi"
+}
+}
+console.log(divisibleBy(10, 5))
 
 // Write a function named assignGrade that takes a number score as an argument and returns the letter grade for the score.
 
