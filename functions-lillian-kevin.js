9366f61d6c5bf458a47182d601b3142859f257b6
silverkd70 <silverkd70@icloud.com>
2021-09-14 14:49:47 -0700

    completed function challenges

--- functions-lillian-kevin.js
+++ functions-lillian-kevin.js
@@ -29,15 +29,36 @@ const triple = (number) => {
 console.log(triple(1))
 
 // Write a function named multiply that takes two numbers as arguments and returns the result of the numbers multiplied together.
-//
-// // Write a function named divisibleBy that takes two numbers as arguments and returns whether the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5".
+
+const multiply = (num1, num2) => {
+  return num1 * num2
+}
+console.log(multiply(3, 5))
+
+// Write a function named divisibleBy that takes two numbers as arguments and returns whether the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5".
+
+const divisibleBy = (num1, num2) => {
+  if (num1 % num2 === 0) {
+    return `${num1} is evenly divisible by ${num2}`    
+  } else {
+    return `${num1} is not evenly divisible ${num2}`
+  }
+}
+console.log(divisibleBy(10, 5))
 //
 // Write a function named assignGrade that takes a number score as an argument and returns the letter grade for the score.
 //
 // Write a function named isLonger that takes two strings as arguments and returns the string that contains the most characters.
+
+const isLonger = (str1, str2) => {
+  if (str1.length > str2.length) {
+    return str1
+  } else if (str2.length > str1.length) {
+    return str2
+}
+}
+console.log(isLonger("lillian", "kevin"))
 //
 // Write a function named greaterNum that takes two numbers as arguments and returns whichever number is the greater (higher) number.
 //
-// Write a function named yelling that takes a string as an argument and return the string in all uppercase case letters.
-//
-// STRETCH Challenges
+// Write a function named yelling that takes a string as an argument and return the string in all uppercase case //////letters.
