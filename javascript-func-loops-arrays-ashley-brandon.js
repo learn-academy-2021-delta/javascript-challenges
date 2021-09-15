fe850c1a34580e4ae7d6ae87611a547f38bd9013
alappie <ashley@specialtyproduce.com>
2021-09-14 17:07:37 -0700

    To Brandon from Ash

--- javascript-func-loops-arrays-ashley-brandon.js
+++ javascript-func-loops-arrays-ashley-brandon.js
@@ -40,3 +40,13 @@
 // Write a function that takes in an array of numbers and letters and returns a string with only the letters. HINT: use the typeof method.
 var comboArr = [7, "n", true, "i", "c", 10, "e", -388, "w", 3, "o", 0, "r", false, "k"]
 // --> "nicework"
+
+
+const string = (array) => {
+let newString = []
+for (let i = 0; i < array.length; i++) {
+   newString.push(typeof String (comboArr))     
+}
+return comboArr
+}
+console.log(string(comboArr));
