ff1c1e0776617d093a49a23ef73295a017662d09
silverkd70 <silverkd70@icloud.com>
2021-09-15 12:03:43 -0700

    completed challenges 2 & 3

--- higher-order-functions-kevin-phil.js
+++ higher-order-functions-kevin-phil.js
@@ -16,15 +16,27 @@ console.log(myArrayNumbers)
 
 
 // Write a function that takes in an array of numbers and returns a new array with only odd numbers.
-// var arr2 = [2, 7, 3, 5, 8, 10, 13]
+var arr2 = [2, 7, 3, 5, 8, 10, 13]
 // // --> [7, 3, 5, 13]
 
-
+const onlyOdds = (array) => {
+  return array.filter(value => {
+    return value % 2 !== 0
+  })
+}
+console.log(onlyOdds(arr2))
 
 
 // Write a function that takes in an array of numbers and letters and returns a string with only the letters. HINT: Use typeof method
-// var comboArr = [7, "n", "i", "c", 10, "e", false, "w", 3, "o", "r", "k"]
+var comboArr = [7, "n", "i", "c", 10, "e", false, "w", 3, "o", "r", "k"]
 // // --> "nicework"
+
+const letterGrab = (array) => {
+  return array.filter(value => {
+    return typeof value === "string"
+  })
+}
+console.log(letterGrab(comboArr))
 
 
 
