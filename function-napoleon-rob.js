878bc77a96acc88ff84ec4e00cbfafb9a8588063
LEARN Academy <learnacademy@LEARNs-Air.lan>
2021-09-14 17:59:37 -0700

    completed assesment

---
+++ function-napoleon-rob.js
@@ -0,0 +1,55 @@
+// Write a function named marco that returns "polo".
+// const Marco = () =>{
+// return"polo"
+// }
+// console.log(Marco ());
+// Write a function named greeting that takes a name as an argument and returns "Welcome, <person's name here>!"
+// const greeting = () => {
+//   return "welcome, jon"
+// }
+// console.log(greeting())
+// Write a function named oddOrEven that takes a number as an argument and returns whether the number is odd or even.
+// const greeting = (oddOrEven) => {
+// return `i ${oddOrEven}!`
+// for (let i=1; 1<=20; i++){
+// if(i%2 === 0){
+// console.log(greeting(i));{
+// if {
+// console.log(greeting("odd"));
+// else {
+// console.log(greeting("even"));
+// }
+// option #2
+// const oddOrEven = (number) => {
+// if(oddOrEven%2 === 0){
+// return "odd"
+// } else if(oddOrEven === 1)
+// return "even"
+// }
+// Write a function named triple that takes a number as an argument and returns the result of that number multiplied by 3.
+// var myArrayOfNums = [2, 3, 4, 5, 6]
+//
+// const triple = (array) => {
+// let numbersMultBy3 = []
+// for(let i=0; i<array.length; i++) {
+// numbersMultBy3.push(array[i] * 3)
+// }
+// console.log(numbersMultBy3);
+// return numbersMultBy3
+// }
+// console.log(triple(myArrayOfNums));
+// console.log(triple([2, 3, 4]));
+// Write a function named multiply that takes two numbers as arguments and returns the result of the numbers multiplied together.
+const multiply = (num1, num2) => {
+return num1 * num2
+}
+console.log(multiply(2, 4))
+// Write a function named divisibleBy that takes two numbers as arguments and returns whether the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5".
+//
+// Write a function named assignGrade that takes a number score as an argument and returns the letter grade for the score.
+//
+// Write a function named isLonger that takes two strings as arguments and returns the string that contains the most characters.
+//
+// Write a function named greaterNum that takes two numbers as arguments and returns whichever number is the greater (higher) number.
+//
+// Write a function named yelling that takes a string as an argument and return the string in all uppercase case letters.
