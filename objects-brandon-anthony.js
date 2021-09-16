2250217581adbcd9a7b9d28f158cf22f3db28fec
brandonho2000 <bradho099@gmail.com>
2021-09-16 13:57:47 -0500

    first question partially done

---
+++ objects-brandon-anthony.js
@@ -0,0 +1,17 @@
+// Consider this variable:
+var person = {
+  firstName: "Arthur",
+  lastName: "Dent"
+}
+// Write the code that accesses the first name of the person object.
+// Write the code that accesses the last name of the person object.
+// Write the code that gives the person object a property of homePlanet and set it to 'Earth'.
+// Update the person object with a method that logs "Arthur Dent is from planet Earth".
+
+const personInfo = (object) => {
+    object.map(value => {
+
+    })
+}
+console.log(person.firstName)
+console.log(person.lastName)
