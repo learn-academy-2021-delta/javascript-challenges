a9528bc61526a6d62e1183e0342edf58d60763a6
JBrady <jbradysd@icloud.com>
2021-09-16 16:01:46 -0700

    finished 4 challenges

--- objects-john-sahtra.js
+++ objects-john-sahtra.js
@@ -1,45 +1,58 @@
 // Consider this variable:
-var person = {
-  firstName: "Arthur",
-  lastName: "Dent",
-  homePlanet: "Earth",
-  getData: function() {
-      return `${this.firstName} ${this.lastName} is from planet ${this.homePlanet}`
-  }
-}
+// var person = {
+//   firstName: "Arthur",
+//   lastName: "Dent",
+//   homePlanet: "Earth",
+//   getData: function() {
+//       return `${this.firstName} ${this.lastName} is from planet ${this.homePlanet}`
+//   }
+// }
 
 // Write the code that accesses the first name of the person object.
 // Write the code that accesses the last name of the person object.
 // Write the code that gives the person object a property of homePlanet and set it to 'Earth'.
 // Update the person object with a method that logs "Arthur Dent is from planet Earth".
 
-console.log(person.firstName);
+// console.log(person.firstName);
 
-console.log(person.lastName);
-console.log(person.getData());
+// console.log(person.lastName);
+// console.log(person.getData());
 
 
 // Consider this variable:
-var product = {
-  name: "chair", price: 14.99
-}
+// var product = {
+//   name: "chair", price: 14.99
+// }
 // Write a function called describeProduct takes product as an argument and logs "The product is a chair. It costs $14.99".
 
-const describeProduct = (product) => {
-    return `The product is a ${product.name}. It costs ${product.price}.`
-}
-console.log(describeProduct(product));
+// const describeProduct = (product) => {
+//     return `The product is a ${product.name}. It costs ${product.price}.`
+// }
+// console.log(describeProduct(product));
+
+
 
 // Consider this variable:
-var lunch = {
-  name: "PB and Banana",
-  type: "sandwich",
-  ingredients: ["bread", "peanut butter", "banana"]
+// var lunch = {
+//   name: "PB and Banana",
+//   type: "sandwich",
+//   ingredients: ["bread", "peanut butter", "banana"],
+//   lunchMenu: function() {return `The ingredients for a ${lunch.name} ${lunch.type} are ${lunch.ingredients[0]}, ${lunch.ingredients[1]}, and ${lunch.ingredients[2]}.`}
 // }
+
 // Write the code that accesses the ingredients property.
+// console.log(lunch.ingredients)
+
 // Write the code that access the 3rd ingredient of the lunch object.
+// console.log(lunch.ingredients[2])
+
 // Write a function that takes the lunch object as an argument and returns "The ingredients for a PB and Banana sandwich are bread, peanut butter, and banana."
+
 // Update the lunch object with method that returns "The ingredients for a PB and Banana sandwich are bread, peanut butter, and banana."
+// console.log(lunch.lunchMenu())
+
+
+
 // Consider this variable:
 // var animals = [
 //   { name: "Waffles", type: "dog", age: 12 },
@@ -48,15 +61,34 @@ var lunch = {
 //   { name: "Hank", type: "cat", age: 11 },
 // ]
 // Create a function that takes in any array of objects and returns a new array with only those of type cat.
+// const cats = animals.filter(value => value.type === "cat")
+// console.log(cats)
+
 // Using the same array of objects above. Create a function that returns a new array with only the names of the animals.
-// Consider this variable:
-// let author = {
-//     name: "H. G. Wells",
-//     genre: "science fiction"
+// const nameGrabber = (array) => {
+//   return array.map(value => {
+//     return value.name 
+//   })
 // }
+
+// console.log(nameGrabber(animals))
+
+
+// Consider this variable:
+let author = {
+    name: "H. G. Wells",
+    genre: "science fiction"
+}
 // Write the code that destructures the author object and makes the following work:
-// console.log(`${name} is a ${genre} author`)
+let {  } author
+
+console.log(`${name} is a ${genre} author`)
 // --> "H. G. Wells is a science fiction author"
+
+
+
+
+
 // Consider this variable:
 // let pokeOne = {
 //     species: "Charmandar",
