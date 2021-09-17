1971d95bf10333893190fb926c19927f094554a1
JBrady <jbradysd@icloud.com>
2021-09-17 11:59:00 -0700

    working on number 3, completed 1 and 2

--- classes-john-sahtra.js
+++ classes-john-sahtra.js
@@ -38,21 +38,64 @@ class Coffee {
   
 //   Write the code that makes a coffee with 2 sugars. Then write the code that outputs the coffee's profile.
   
-var verySweetCoffee = new Coffee("Pumpkin Spice Latte", 0, 2)
-console.log(verySweetCoffee.coffeeProfile());
+// var verySweetCoffee = new Coffee("Pumpkin Spice Latte", 0, 2)
+// console.log(verySweetCoffee.coffeeProfile());
 
 
 //   Latte Maker
 //   Write a Latte class that receives a flavor, a milk type and a number of shots.
+
+class LatteMaker {
+  constructor(flavor, milk, shot) {
+    this.flavor = flavor,
+    this.milk = milk,
+    this.shot = shot
+  }
+
+  latteProfile(){
+    return(`${this.flavor}: ${this.milk}, ${this.shots()}`)
+  }
+
+  shots(){
+    if (this.shot > 1) {
+      return `${this.shot} shots`
+    } else {
+      return `${this.shot} shot`
+    }
+  }
+}
+
+// var mochaLatte = new LatteMaker("mocha","oat milk", 15)
+// console.log(mochaLatte.latteProfile())
+// var regularLatte = new LatteMaker("regular", "no milk", 1)
+// console.log(regularLatte.latteProfile())
+var hazelnutLatte = new LatteMaker("Hazelnut", "Almond Milk", 2)
+console.log(hazelnutLatte.latteProfile())
   
 //   Write a method for your Latte class that outputs the latte's profile.
-  
 //   Write the code that makes a regular, single shot latte. Then, log the latte's profile.
-  
 //   Write the code that makes a double shot hazelnut latte with almond milk. Then, log the latte's profile.
   
 //   Volume of a Cylinder
 //   Write a class that calculates the volume of a Cylinder to four decimal places. Volume of a cylinder : V = πr2h (r is the radius and h is the height of the cylinder)
+class cylinderVolume {
+  constructor(radius, height) {
+    this.radius = radius ** 2,
+    this.height = height
+  }
+
+  latteProfile(){
+    return(`${this.flavor}: ${this.milk}, ${this.shots()}`)
+  }
+
+  shots(){
+    if (this.shot > 1) {
+      return `${this.shot} shots`
+    } else {
+      return `${this.shot} shot`
+    }
+  }
+}
   
 //   Write the code that creates three unique cylinder objects
   
